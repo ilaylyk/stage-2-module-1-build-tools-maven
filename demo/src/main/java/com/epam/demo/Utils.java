ad8cfd095b77a8d0cb276b863ef0f504070a21aa
ilaylyk <ilay_lyk@mail.ru>
2022-11-11 02:24:32 +0300

    Maven

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,12 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        return args.stream().filter(Predicate.not(StringUtils::isPositiveNumber)).findFirst().isEmpty();
     }
 }
